17070bda4b8a2418f095df33ee9b380137d19842
gauri Sonawane <gaurisonawane918@gmail.com>
2025-07-26 18:46:10 +0530

    Route for sending messages

--- next-auth/src/app/api/get-messages/route.ts
+++ next-auth/src/app/api/get-messages/route.ts
@@ -3,6 +3,7 @@ import { authOptions } from "../auth/[...nextauth]/options";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import {User} from "next-auth";
+import mongoose from "mongoose";
 
 export async function GET (request:Request){
     await dbConnect()
@@ -18,5 +19,40 @@ export async function GET (request:Request){
             },{status:401}
         )
     }
-    const userId = user._id;
+    const userId = new mongoose.Types.ObjectId(user._id);
+    try {
+        const user = await UserModel.aggregate([
+            {$match: {id:userId}},
+            {$unwind: '$messages'},
+            {$sort: {'messages.createdAt': -1}},
+            {$group : {_id: '$_id',messages:{$push:'$messages'}}}
+        ])
+        if (!user || user.length === 0) {
+            return Response.json(
+            {
+                success:false,
+                message:'User not found',
+            },
+            {status:401}
+        )
+    }
+    return Response.json(
+            {
+                success:true,
+                messages:user[0].messages
+            },
+            {status:200}
+        )
+    } catch (error) {
+        console.log("An unexpected error occured:",error);
+        
+        return Response.json(
+                {
+                    success:false,
+                    message: "Not authenticated"
+                },
+                {status : 500}
+            )
+        
+    }
 }
